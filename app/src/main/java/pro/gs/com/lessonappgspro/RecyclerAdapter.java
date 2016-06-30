25c977ba796b029e552e2c08665206986dd82686
竹野学 <gakunet54@gmail.com>
2016-06-30 22:21:49 +0900

    no message

--- app/src/main/java/pro/gs/com/lessonappgspro/RecyclerAdapter.java
+++ app/src/main/java/pro/gs/com/lessonappgspro/RecyclerAdapter.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter {
+
     private ArrayList<CustomDataObject> my_List;
     private Context context;
 
@@ -23,25 +24,19 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        //リスト一つ分のレイアウトを取り出す
         View view = LayoutInflater.from(context).inflate(R.layout.customlistview, parent, false);
         return new ViewHolder(view, context);
     }
 
+
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
         ViewHolder holderView = (ViewHolder) holder;
-
-        //リスト項目一つ分のオブジェクトデータ（CutomDataObject）を取り出す。
+        //現在、画面に現れたデータを得る
         CustomDataObject item = my_List.get(position);
-
-        //レイアウトにデータを流し込み、一つ分のレイアウトを作成する。
-        //onBindViewHolder関数の処理が終わる同時にレイアウトがリストに追加される
         holderView.name.setText(item.getName());
         holderView.image.setImageResource(item.getImageId());
-
-
 
     }
 
@@ -49,6 +44,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
     public int getItemCount() {
         return this.my_List.size();
     }
+
 
     // 毎回findViewByIdをしなくてよくし、高速化に使用するholderクラス
     private static class ViewHolder extends RecyclerView.ViewHolder {
@@ -59,6 +55,9 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
             // CustomDataのデータをViewの各Widgetにセットする
             name = (TextView) v.findViewById(R.id.name);
             image = (ImageView) v.findViewById(R.id.image);
+
         }
+
     }
+
 }
