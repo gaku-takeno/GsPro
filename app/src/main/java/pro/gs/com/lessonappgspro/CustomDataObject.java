aa45b661faad048414cff2e544bc49c6f17ca89b
竹野学 <gakunet54@gmail.com>
2016-06-30 23:13:51 +0900

    no message

---
+++ app/src/main/java/pro/gs/com/lessonappgspro/CustomDataObject.java
@@ -0,0 +1,29 @@
+package pro.gs.com.lessonappgspro;
+
+
+
+
+/**
+ * Created by takenogaku on 2016/05/05.
+ */
+public class CustomDataObject {
+
+    private String name;
+    private int imageId;
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setImageId (int imageId) {
+        this.imageId = imageId;
+    }
+
+    public int getImageId() {
+        return this.imageId;
+    }
+}
