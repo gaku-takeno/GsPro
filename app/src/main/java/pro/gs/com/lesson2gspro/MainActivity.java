23ed5e435733eda7772fdc31717e81473c348cf5
竹野学 <gakunet54@gmail.com>
2016-07-02 16:25:12 +0900

    no message

---
+++ app/src/main/java/pro/gs/com/lesson2gspro/MainActivity.java
@@ -0,0 +1,110 @@
+package pro.gs.com.lesson2gspro;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //型がTextViewであるnameViewという名前の変数を宣言している。
+    //この時点では、nameViewには何も入っていない。つまりNULL
+    private TextView greetingView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //viewオブジェクトを取り出して、メンバー変数に格納する。
+        greetingView = (TextView) findViewById(R.id.greeting);
+        TextView greetingView2 = greetingView;
+
+        greetingView.setText("wここをクリックして挨拶する!");
+
+        greetingView.setText("クリックして自分を創ってみる!");
+
+        //アクション（インテントに画面遷移の処理）を実装
+        setActions();
+
+    }
+
+
+    private void setActions(){
+
+        // クリック時の処理
+        greetingView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intentTrans();
+            }
+        });
+
+        //以下のような書き方も出来る。
+//        View.OnClickListener myClickListner = new View.OnClickListener() {
+//            public void onClick(View v) {
+//                intentTrans();
+//            }
+//        };
+//        // クリック時の処理
+//        greetingView.setOnClickListener(myClickListner);
+
+
+    }
+
+    private void intentTrans () {
+
+
+        EditText firstNameView = (EditText) findViewById(R.id.firstname);
+        EditText familyNameView = (EditText) findViewById(R.id.familyname);
+
+        EditText ageView = (EditText) findViewById(R.id.age);
+
+        String firstname = firstNameView.getText().toString();
+        String familyname = familyNameView.getText().toString();
+        String ageTxt = ageView.getText().toString();
+        int age = Integer.parseInt(ageTxt);
+
+        //1:男 0:女
+        int sex = 1;
+
+        // ラジオグループのオブジェクトを取得
+        RadioGroup rg = (RadioGroup)findViewById(R.id.radiogroup_sex);
+        // チェックされているラジオボタンの ID を取得
+        RadioButton radio_male = (RadioButton)findViewById(R.id.radio_male);
+//        RadioButton radio_female = (RadioButton)findViewById(R.id.radio_female);
+        if(radio_male.isChecked()) {
+            sex = 1;
+        } else {
+            sex = 0;
+        }
+
+        if (!firstname.equals("") && !familyname.equals("")) {
+
+            //ここでSubActivityへ画面遷移する。
+            Intent intent = new Intent(this, SubActivity.class);
+            intent.putExtra("first_name", firstname);
+            intent.putExtra("family_name", familyname);
+            intent.putExtra("sex",sex);
+            intent.putExtra("age",age);
+            startActivity(intent);
+
+        } else {
+
+            // 第3引数は、表示期間（LENGTH_SHORT、または、LENGTH_LONG）
+            Toast.makeText(this, "名前と苗字を入れてください。", Toast.LENGTH_LONG).show();
+
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/pro/gs/com/lesson2gspro/MySelf.java
@@ -0,0 +1,175 @@
+package pro.gs.com.lesson2gspro;
+
+
+
+import android.content.Context;
+
+import pro.gs.com.lib.Greeting;
+import pro.gs.com.lib.Location;
+
+
+public class MySelf {
+
+    private Context context;
+    private String hobby;
+
+    private String name;
+
+    private int age;
+
+    private int sex;//1:男 0:女
+
+    private Location location;
+
+    /**
+     *コンストラクタ
+     */
+    public MySelf (Context context) {
+        this.age = 0;
+        this.context = context;
+    }
+
+    /**
+     * 自己紹介をする
+     * @return
+     */
+    public String doSelftIntruductionBasic() {
+
+        String greeting = "";
+
+        if (this.name != null) {
+            greeting += "私は"+this.name + "です。\n";
+        }
+
+
+
+        greeting += this.age + "歳です。\n";
+
+        if (this.hobby != null) {
+            greeting += "趣味は"+this.hobby + "です。\n";
+        }
+
+        if (this.sex == 1) {
+            greeting += "男です。よろしく！！";
+        } else {
+            greeting += "女です。よろしくね〜";
+        }
+
+        // 第3引数は、表示期間（LENGTH_SHORT、または、LENGTH_LONG）
+//        Toast.makeText(this.context, greeting, Toast.LENGTH_LONG).show();
+
+        return greeting;
+    }
+
+
+    /**
+     * 自己紹介をする
+     * @return
+     */
+    public String doSelftIntruduction() {
+
+        Greeting greetingObj = new Greeting(location.getLang());
+        String greeting = greetingObj.doGreeting()+"\n";
+
+        if (this.name != null) {
+            greeting += "私は"+this.name + "です。\n";
+        }
+
+        if (location.getArea() != null) {
+            greeting += "現在私は、"+location.getArea() + "にいます。\n";
+        }
+
+        if (location.getLocalTime() != null) {
+            greeting += "日付は、"+location.getLocalTime() + "になります。\n";
+        }
+
+        if (location.getLangJp() != null) {
+            greeting += "普段、"+location.getLangJp() + "を話します。\n";
+        }
+
+        greeting += this.age + "歳です。\n";
+
+        if (this.hobby != null) {
+            greeting += "趣味は"+this.hobby + "です。\n";
+        }
+
+        if (this.sex == 1) {
+            greeting += "男です。よろしく！！";
+        } else {
+            greeting += "女です。よろしくね〜";
+        }
+
+        // 第3引数は、表示期間（LENGTH_SHORT、または、LENGTH_LONG）
+//        Toast.makeText(this.context, greeting, Toast.LENGTH_LONG).show();
+
+        return greeting;
+    }
+
+
+    /**
+     * 名前を設定する
+     * @param name
+     */
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    /**
+     * 名前を取る
+     * @return
+     */
+    public String getName () {
+        return this.name;
+    }
+
+    /**
+     * 趣味を設定する
+     * @param hobby
+     */
+    public void setHobby (String hobby) {
+        this.hobby = hobby;
+    }
+
+
+
+    public void setLocation (Location location) {
+        this.location = location;
+    }
+
+    /**
+     * 性別を設定する
+     * @param sex
+     */
+    public void setSex (int sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * 性別を取る
+     * 1:男 0:女
+     * @return
+     */
+    public int getSex () {
+        return this.sex;
+    }
+
+
+    /**
+     * 年齢を設定する
+     * @param age
+     */
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    /**
+     * 年齢を取る
+     * @return
+     */
+    public int getAge () {
+        return this.age;
+    }
+
+
+
+}
