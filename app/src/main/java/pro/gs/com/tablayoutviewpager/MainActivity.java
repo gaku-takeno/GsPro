5d784efa5015e8fbb761b82081375506552134b1
竹野学 <gakunet54@gmail.com>
2016-07-10 16:02:34 +0900

    no message

---
+++ app/src/main/java/pro/gs/com/tablayoutviewpager/MainActivity.java
@@ -0,0 +1,29 @@
+package pro.gs.com.tablayoutviewpager;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pro.gs.com.tablayoutviewpager.adapter.SampleFragmentPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(), this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        //TabLayoutでタブを均等に配置する
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+    }
+}
